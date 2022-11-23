2aca8bd0f2d94e8dfbffdb78a16d5e0306be816f
igorjba <igorjbahia@gmail.com>
2022-11-23 13:29:58 -0300

    :sparkles: feat: Exercício 03 resolvido.

--- 03/index.js
+++ 03/index.js
@@ -1,1 +1,9 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
+
+function replaceAll(original, text, newText) {
+    while ((original !== original.replace(text, newText))) {
+        original = original.replace(text, newText);
+    }
+    return original;
+}
+console.log(replaceAll(texto, " ", "-"));
