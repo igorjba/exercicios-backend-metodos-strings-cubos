2120491663cc2589c7175925b94f13983de1cc2c
igorjba <igorjbahia@gmail.com>
2022-11-23 14:06:39 -0300

    :sparkles: feat: Exercício 05 resolvido.

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,8 @@
 const numeroCartao = '1111222233334444';
+
+let cartaoTermo1 = numeroCartao.slice(0, 4);
+let cartaoTermo2 = numeroCartao.slice(12, 16);
+
+let cartao = cartaoTermo1 + cartaoTermo2.padStart(12, "*");
+
+console.log(cartao);
