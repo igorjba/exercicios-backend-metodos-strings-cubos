88ced248c62fecc5becd82d3a65d18a4c759411f
igorjba <igorjbahia@gmail.com>
2022-11-24 07:52:49 -0300

    :sparkles: feat: Exercício 08 resolvido.

--- 08/index.js
+++ 08/index.js
@@ -1,1 +1,16 @@
-const cpf = "011.022.033-44";
+const cpf = "011.022.03344";
+
+function removerPontuacaoCpfCnpj(original, text, newText) {
+    while (original !== original.replace(".", "")) { //while = enquanto
+        original = original.replace(".", "");
+    }
+    while (original !== original.replace("-", "")) { //while = enquanto
+        original = original.replace("-", "");
+    }
+    while (original !== original.replace("/", "")) { //while = enquanto
+        original = original.replace("/", "");
+    }
+    return console.log(original);
+}
+
+removerPontuacaoCpfCnpj(cpf);
