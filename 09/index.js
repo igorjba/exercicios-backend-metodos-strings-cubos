8e7ddaa85429c1614ff09ce0d33a4c807f5bdf23
igorjba <igorjbahia@gmail.com>
2022-11-24 09:45:40 -0300

    :sparkles: fear: Exercício 09 resolvido.

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,47 @@
-const nome = 'Guido Cerqueira';
+const nome = 'Guido';
+
+function retirarEspacos(nomeOriginal) {
+    if (nomeOriginal.includes(" ")) {
+        while (nomeOriginal !== nomeOriginal.replace(" ", "")) {
+            nomeOriginal = nomeOriginal.replace(" ", "")
+            nomeTudoJunto = nomeOriginal;
+        }
+    } else {
+        nomeTudoJunto = nomeOriginal;
+    }
+
+}
+
+let nomeTudoJunto
+
+function criarNick(nomeOriginal) {
+
+    if (nomeOriginal.length > 0) {
+
+        let nomeJuntoMinusculo = nomeTudoJunto.toLowerCase();
+
+        if (nomeJuntoMinusculo.length < 12) {
+
+            let nomeJuntoMinusculo12Carac = nomeJuntoMinusculo.padEnd(12, (Math.floor(Math.random() * 10)));
+
+            return console.log(`@${nomeJuntoMinusculo12Carac}`);
+
+        } else {
+
+            nomeJuntoMinusculo = nomeJuntoMinusculo.slice(0, 12);
+
+            return console.log(`@${nomeJuntoMinusculo}`);
+        }
+
+
+
+    } else {
+
+        return console.log("Nome Inválido");
+
+    }
+
+}
+retirarEspacos(nome)
+criarNick(nome);
+
