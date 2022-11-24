ecbf8faaf31d78b14e4ef9f488a14079926abe1c
igorjba <igorjbahia@gmail.com>
2022-11-24 11:08:10 -0300

    :sparkles: feat: Exercício 10 resolvido.

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,46 @@
-const nomeArquivo = 'Foto da Familia.pdf';
+const nomeArquivo = 'Foto da Familia.jpg';
+
+function validacaoDeArquivos(arquivo) {
+
+    const tiposDeArquivos = ["jpg", "jpeg", "gif", "png"];
+
+    let indice = arquivo.length - 4;
+
+    let indiceDoPonto = arquivo.indexOf(".", indice);
+
+    let arquivoValido = false;
+
+    if (indiceDoPonto === indice) {
+
+        let tipoDeAquivo = nomeArquivo.slice(indice + 1);
+
+        for (let i = 0; i < tiposDeArquivos.length; i++) {
+
+            if (tipoDeAquivo === tiposDeArquivos[i]) {
+
+                arquivoValido = true;
+
+            } else {
+
+                arquivoValido = false;
+
+            }
+        }
+
+    } else {
+
+        arquivoValido = false;
+
+    }
+    if (arquivoValido === true) {
+
+        return console.log("Arquivo válido");
+
+    } else {
+
+        return console.log("Arquivo inválido");
+
+    }
+}
+
+validacaoDeArquivos(nomeArquivo);
