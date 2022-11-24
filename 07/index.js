8bd27555d4d6a3d11ebd33072db0a64672527030
igorjba <igorjbahia@gmail.com>
2022-11-24 07:40:12 -0300

    :sparkles: feat: Exercício 07 resolvido.

--- 07/index.js
+++ 07/index.js
@@ -1,1 +1,41 @@
 const email = "aluno@cubos.academy";
+
+function verificarEmail(enderecoEmail) {
+    if (enderecoEmail.includes("@")) {
+
+        if (enderecoEmail.includes(".")) {
+
+            for (let i = 0; i < enderecoEmail.length; i++) {
+
+                if (enderecoEmail[enderecoEmail.length] === (".") || enderecoEmail[0] === ".") {
+
+                    return console.log("E-mail inválido");
+
+                } else {
+
+                    if (enderecoEmail.indexOf(".", enderecoEmail.indexOf("@"))) {
+
+                        return console.log("E-mail válido");
+
+                    } else {
+
+                        return console.log("E-mail inválido");
+
+                    }
+
+                }
+            }
+        } else {
+
+            return console.log("E-mail inválido");
+
+        }
+
+    } else {
+
+        return console.log("E-mail inválido");
+
+    }
+}
+
+verificarEmail(email);
