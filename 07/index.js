c548cebcce84cb0aa172d629ea4b4ec4118b896e
igorjba <igorjbahia@gmail.com>
2022-12-09 07:25:37 -0300

    :recycle: refactor: Código refeito com o intuito de deixá-lo mais dry.

--- 07/index.js
+++ 07/index.js
@@ -1,41 +1,51 @@
 const email = "aluno@cubos.academy";
 
-function verificarEmail(enderecoEmail) {
-    if (enderecoEmail.includes("@")) {
+function validarEmail(email) {
+    let emailValido = true;
 
-        if (enderecoEmail.includes(".")) {
-
-            for (let i = 0; i < enderecoEmail.length; i++) {
-
-                if (enderecoEmail[enderecoEmail.length] === (".") || enderecoEmail[0] === ".") {
-
-                    return console.log("E-mail inválido");
-
-                } else {
-
-                    if (enderecoEmail.indexOf(".", enderecoEmail.indexOf("@"))) {
-
-                        return console.log("E-mail válido");
-
-                    } else {
-
-                        return console.log("E-mail inválido");
-
-                    }
-
-                }
-            }
-        } else {
-
-            return console.log("E-mail inválido");
-
-        }
-
-    } else {
-
-        return console.log("E-mail inválido");
-
+    if (email.indexOf("@") < 0) {
+        emailValido = false;
+    } else if (email.indexOf(".") < 0) {
+        emailValido = false;
+    } else if (email.indexOf(".") === 0) {
+        emailValido = false;
+    } else if (email.lastIndexOf(".") === (email.length - 1)) {
+        emailValido = false;
     }
+
+    if (emailValido) {
+        console.log("email válido");
+    } else {
+        console.log("email inválido");
+    }
+
 }
 
-verificarEmail(email);
+validarEmail("jose@cubos.academy");
+validarEmail("jose@cubos.academy.br");
+validarEmail("jose.messias@cubos.academy");
+validarEmail("jose.messias@cubos.io");
+validarEmail("jose@cubos");
+validarEmail("jose.messias@cubos");
+validarEmail("jose.messias@.");
+validarEmail("jose.@cubos");
+validarEmail(".@");
+validarEmail("@.");
+validarEmail("jose.messias@cubos.");
+validarEmail(".messias@cubos.");
+validarEmail(".messias@cubos");
+
+
+// | `jose@cubos.academy`         | E-mail válido   |
+// | `jose@cubos.academy.br`      | E-mail válido   |
+// | `jose.messias@cubos.academy` | E-mail válido   |
+// | `jose.messias@cubos.io`      | E-mail válido   |
+// | `jose@cubos`                 | E-mail inválido |
+// | `jose.messias@cubos`         | E-mail válido   |
+// | `jose.messias@.`             | E-mail inválido |
+// | `jose.@cubos`                | E-mail válido   |
+// | `.@`                         | E-mail inválido |
+// | `@.`                         | E-mail inválido |
+// | `jose.messias@cubos.`        | E-mail inválido |
+// | `.messias@cubos.`            | E-mail inválido |
+// | `.messias@cubos`             | E-mail inválido |
